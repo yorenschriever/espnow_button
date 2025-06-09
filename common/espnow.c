55ea577adb5ced0e02878a3c370e544c4ec875d6
Yoren Schriever <yoren@ocelot.nl>
2025-06-09 09:55:37 +0200

    code cleanup

--- common/espnow.c
+++ common/espnow.c
@@ -5,17 +5,11 @@
 #include <time.h>
 #include <string.h>
 #include <assert.h>
-
 #include "espnow.h"
 
 static const char *TAG = "espnow";
 
-static QueueHandle_t s_example_espnow_queue;
-
 static uint8_t s_example_broadcast_mac[ESP_NOW_ETH_ALEN] = { 0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF };
-// static uint16_t s_example_espnow_seq[EXAMPLE_ESPNOW_DATA_MAX] = { 0, 0 };
-
-// static void example_espnow_deinit(example_espnow_send_param_t *send_param);
 
 void example_wifi_init(void)
 {
@@ -28,177 +22,23 @@ void example_wifi_init(void)
     ESP_ERROR_CHECK( esp_wifi_start());
     ESP_ERROR_CHECK( esp_wifi_set_channel(CONFIG_ESPNOW_CHANNEL, WIFI_SECOND_CHAN_NONE));
 
-// #if CONFIG_ESPNOW_ENABLE_LONG_RANGE
+    //enable long range
     ESP_ERROR_CHECK( esp_wifi_set_protocol(ESPNOW_WIFI_IF, WIFI_PROTOCOL_11B|WIFI_PROTOCOL_11G|WIFI_PROTOCOL_11N|WIFI_PROTOCOL_LR) );
-// #endif
 }
 
-
-// static void example_espnow_task(void *pvParameter)
-// {
-//     example_espnow_event_t evt;
-//     uint8_t recv_state = 0;
-//     uint16_t recv_seq = 0;
-//     int recv_magic = 0;
-//     bool is_broadcast = false;
-//     int ret;
-
-//     vTaskDelay(5000 / portTICK_PERIOD_MS);
-//     ESP_LOGI(TAG, "Start sending broadcast data");
-
-//     // /* Start sending broadcast ESPNOW data. */
-//     // example_espnow_send_param_t *send_param = (example_espnow_send_param_t *)pvParameter;
-//     // if (esp_now_send(send_param->dest_mac, send_param->buffer, send_param->len) != ESP_OK) {
-//     //     ESP_LOGE(TAG, "Send error");
-//     //     example_espnow_deinit(send_param);
-//     //     vTaskDelete(NULL);
-//     // }
-
-//     while (xQueueReceive(s_example_espnow_queue, &evt, portMAX_DELAY) == pdTRUE) {
-//         switch (evt.id) {
-//             case EXAMPLE_ESPNOW_SEND_CB:
-//             {
-//                 example_espnow_event_send_cb_t *send_cb = &evt.info.send_cb;
-//                 is_broadcast = IS_BROADCAST_ADDR(send_cb->mac_addr);
-
-//                 ESP_LOGD(TAG, "Send data to "MACSTR", status1: %d", MAC2STR(send_cb->mac_addr), send_cb->status);
-
-//                 if (is_broadcast && (send_param->broadcast == false)) {
-//                     break;
-//                 }
-
-//                 if (!is_broadcast) {
-//                     send_param->count--;
-//                     if (send_param->count == 0) {
-//                         ESP_LOGI(TAG, "Send done");
-//                         example_espnow_deinit(send_param);
-//                         vTaskDelete(NULL);
-//                     }
-//                 }
-
-//                 /* Delay a while before sending the next data. */
-//                 if (send_param->delay > 0) {
-//                     vTaskDelay(send_param->delay/portTICK_PERIOD_MS);
-//                 }
-
-//                 ESP_LOGI(TAG, "send data to "MACSTR"", MAC2STR(send_cb->mac_addr));
-
-//                 memcpy(send_param->dest_mac, send_cb->mac_addr, ESP_NOW_ETH_ALEN);
-//                 example_espnow_data_prepare(send_param);
-
-//                 /* Send the next data after the previous data is sent. */
-//                 if (esp_now_send(send_param->dest_mac, send_param->buffer, send_param->len) != ESP_OK) {
-//                     ESP_LOGE(TAG, "Send error");
-//                     example_espnow_deinit(send_param);
-//                     vTaskDelete(NULL);
-//                 }
-//                 break;
-//             }
-//             case EXAMPLE_ESPNOW_RECV_CB:
-//             {
-//                 example_espnow_event_recv_cb_t *recv_cb = &evt.info.recv_cb;
-
-//                 ret = example_espnow_data_parse(recv_cb->data, recv_cb->data_len, &recv_state, &recv_seq, &recv_magic);
-//                 free(recv_cb->data);
-//                 if (ret == EXAMPLE_ESPNOW_DATA_BROADCAST) {
-//                     ESP_LOGI(TAG, "Receive %dth broadcast data from: "MACSTR", len: %d", recv_seq, MAC2STR(recv_cb->mac_addr), recv_cb->data_len);
-
-//                     /* If MAC address does not exist in peer list, add it to peer list. */
-//                     if (esp_now_is_peer_exist(recv_cb->mac_addr) == false) {
-//                         esp_now_peer_info_t *peer = malloc(sizeof(esp_now_peer_info_t));
-//                         if (peer == NULL) {
-//                             ESP_LOGE(TAG, "Malloc peer information fail");
-//                             example_espnow_deinit(send_param);
-//                             vTaskDelete(NULL);
-//                         }
-//                         memset(peer, 0, sizeof(esp_now_peer_info_t));
-//                         peer->channel = CONFIG_ESPNOW_CHANNEL;
-//                         peer->ifidx = ESPNOW_WIFI_IF;
-//                         peer->encrypt = true;
-//                         memcpy(peer->lmk, CONFIG_ESPNOW_LMK, ESP_NOW_KEY_LEN);
-//                         memcpy(peer->peer_addr, recv_cb->mac_addr, ESP_NOW_ETH_ALEN);
-//                         ESP_ERROR_CHECK( esp_now_add_peer(peer) );
-//                         free(peer);
-//                     }
-
-//                     /* Indicates that the device has received broadcast ESPNOW data. */
-//                     if (send_param->state == 0) {
-//                         send_param->state = 1;
-//                     }
-
-//                     /* If receive broadcast ESPNOW data which indicates that the other device has received
-//                      * broadcast ESPNOW data and the local magic number is bigger than that in the received
-//                      * broadcast ESPNOW data, stop sending broadcast ESPNOW data and start sending unicast
-//                      * ESPNOW data.
-//                      */
-//                     if (recv_state == 1) {
-//                         /* The device which has the bigger magic number sends ESPNOW data, the other one
-//                          * receives ESPNOW data.
-//                          */
-//                         if (send_param->unicast == false && send_param->magic >= recv_magic) {
-//                     	    ESP_LOGI(TAG, "Start sending unicast data");
-//                     	    ESP_LOGI(TAG, "send data to "MACSTR"", MAC2STR(recv_cb->mac_addr));
-
-//                     	    /* Start sending unicast ESPNOW data. */
-//                             memcpy(send_param->dest_mac, recv_cb->mac_addr, ESP_NOW_ETH_ALEN);
-//                             example_espnow_data_prepare(send_param);
-//                             if (esp_now_send(send_param->dest_mac, send_param->buffer, send_param->len) != ESP_OK) {
-//                                 ESP_LOGE(TAG, "Send error");
-//                                 example_espnow_deinit(send_param);
-//                                 vTaskDelete(NULL);
-//                             }
-//                             else {
-//                                 send_param->broadcast = false;
-//                                 send_param->unicast = true;
-//                             }
-//                         }
-//                     }
-//                 }
-//                 else if (ret == EXAMPLE_ESPNOW_DATA_UNICAST) {
-//                     ESP_LOGI(TAG, "Receive %dth unicast data from: "MACSTR", len: %d", recv_seq, MAC2STR(recv_cb->mac_addr), recv_cb->data_len);
-
-//                     /* If receive unicast ESPNOW data, also stop sending broadcast ESPNOW data. */
-//                     send_param->broadcast = false;
-//                 }
-//                 else {
-//                     ESP_LOGI(TAG, "Receive error data from: "MACSTR"", MAC2STR(recv_cb->mac_addr));
-//                 }
-//                 break;
-//             }
-//             default:
-//                 ESP_LOGE(TAG, "Callback type error: %d", evt.id);
-//                 break;
-//         }
-//     }
-// }
-
- esp_err_t example_espnow_init(esp_now_send_cb_t send_cb, esp_now_recv_cb_t recv_cb, bool create_queue_task)
+esp_err_t example_espnow_init(esp_now_send_cb_t send_cb, esp_now_recv_cb_t recv_cb)
 {
-    // example_espnow_send_param_t *send_param;
-
-    if (create_queue_task)
-    {
-        s_example_espnow_queue = xQueueCreate(ESPNOW_QUEUE_SIZE, sizeof(example_espnow_event_t));
-        if (s_example_espnow_queue == NULL) {
-            ESP_LOGE(TAG, "Create mutex fail");
-            return ESP_FAIL;
-        }
-    }
-
     /* Initialize ESPNOW and register sending and receiving callback function. */
     ESP_ERROR_CHECK( esp_now_init() );
-    if (send_cb != NULL) {
-        ESP_ERROR_CHECK( esp_now_register_send_cb(send_cb) );
-    }
-    if (recv_cb != NULL) {
-        ESP_ERROR_CHECK( esp_now_register_recv_cb(recv_cb) );
-    }
-    // ESP_ERROR_CHECK( esp_now_register_send_cb(example_espnow_send_cb) );
-    // ESP_ERROR_CHECK( esp_now_register_recv_cb(example_espnow_recv_cb) );
+    if (send_cb != NULL) ESP_ERROR_CHECK( esp_now_register_send_cb(send_cb) );
+    if (recv_cb != NULL) ESP_ERROR_CHECK( esp_now_register_recv_cb(recv_cb) );
+
+    //TODO remove?
 #if CONFIG_ESPNOW_ENABLE_POWER_SAVE
     ESP_ERROR_CHECK( esp_now_set_wake_window(CONFIG_ESPNOW_WAKE_WINDOW) );
     ESP_ERROR_CHECK( esp_wifi_connectionless_module_set_wake_interval(CONFIG_ESPNOW_WAKE_INTERVAL) );
 #endif
+
     /* Set primary master key. */
     ESP_ERROR_CHECK( esp_now_set_pmk((uint8_t *)CONFIG_ESPNOW_PMK) );
 
@@ -206,7 +46,7 @@ void example_wifi_init(void)
     esp_now_peer_info_t *peer = malloc(sizeof(esp_now_peer_info_t));
     if (peer == NULL) {
         ESP_LOGE(TAG, "Malloc peer information fail");
-        vSemaphoreDelete(s_example_espnow_queue);
+        // vSemaphoreDelete(s_example_espnow_queue);
         esp_now_deinit();
         return ESP_FAIL;
     }
@@ -218,102 +58,10 @@ void example_wifi_init(void)
     ESP_ERROR_CHECK( esp_now_add_peer(peer) );
     free(peer);
 
-    // /* Initialize sending parameters. */
-    // send_param = malloc(sizeof(example_espnow_send_param_t));
-    // if (send_param == NULL) {
-    //     ESP_LOGE(TAG, "Malloc send parameter fail");
-    //     vSemaphoreDelete(s_example_espnow_queue);
-    //     esp_now_deinit();
-    //     return ESP_FAIL;
-    // }
-    // memset(send_param, 0, sizeof(example_espnow_send_param_t));
-    // send_param->unicast = false;
-    // send_param->broadcast = true;
-    // send_param->state = 0;
-    // send_param->magic = esp_random();
-    // send_param->count = CONFIG_ESPNOW_SEND_COUNT;
-    // send_param->delay = CONFIG_ESPNOW_SEND_DELAY;
-    // send_param->len = CONFIG_ESPNOW_SEND_LEN;
-    // send_param->buffer = malloc(CONFIG_ESPNOW_SEND_LEN);
-    // if (send_param->buffer == NULL) {
-    //     ESP_LOGE(TAG, "Malloc send buffer fail");
-    //     free(send_param);
-    //     vSemaphoreDelete(s_example_espnow_queue);
-    //     esp_now_deinit();
-    //     return ESP_FAIL;
-    // }
-    // memcpy(send_param->dest_mac, s_example_broadcast_mac, ESP_NOW_ETH_ALEN);
-    // //example_espnow_data_prepare(send_param);
-
-    // // example_espnow_send_param_t *send_param = (example_espnow_send_param_t *)pvParameter;
-    // if (esp_now_send(send_param->dest_mac, send_param->buffer, send_param->len) != ESP_OK) {
-    //     ESP_LOGE(TAG, "Send error");
-    //     example_espnow_deinit(send_param);
-    //     vTaskDelete(NULL);
-    // }
-
-    // if (create_queue_task) {
-    //     /* Create a task to handle ESPNOW sending. */
-    //     xTaskCreate(example_espnow_task, "example_espnow_task", 2048, send_param, 4, NULL);
-    //     ESP_LOGI(TAG, "Create example espnow task");
-    // }
-
     return ESP_OK;
 }
 
- void example_espnow_deinit(example_espnow_send_param_t *send_param)
-{
-    free(send_param->buffer);
-    free(send_param);
-    vSemaphoreDelete(s_example_espnow_queue);
-    esp_now_deinit();
-}
-
-// int espnow_queue_event(example_espnow_event_t *evt)
-// {
-//     // if (xQueueSend(s_example_espnow_queue, evt, ESPNOW_MAXDELAY) != pdTRUE) {
-//     //     ESP_LOGW(TAG, "Send send queue fail");
-//     // }
-//     return xQueueSend(s_example_espnow_queue, evt, ESPNOW_MAXDELAY);
-// }
-
 esp_err_t espnow_broadcast(uint8_t *buffer, int len)
 {
-    // example_espnow_send_param_t *send_param;
-
-    // /* Initialize sending parameters. */
-    // send_param = malloc(sizeof(example_espnow_send_param_t));
-    // if (send_param == NULL) {
-    //     ESP_LOGE(TAG, "Malloc send parameter fail");
-    //     vSemaphoreDelete(s_example_espnow_queue);
-    //     esp_now_deinit();
-    //     return ESP_FAIL;
-    // }
-    // memset(send_param, 0, sizeof(example_espnow_send_param_t));
-    // send_param->unicast = false;
-    // send_param->broadcast = true;
-    // send_param->state = 0;
-    // send_param->magic = esp_random();
-    // send_param->count = CONFIG_ESPNOW_SEND_COUNT;
-    // send_param->delay = CONFIG_ESPNOW_SEND_DELAY;
-    // send_param->len = CONFIG_ESPNOW_SEND_LEN;
-    // send_param->buffer = malloc(CONFIG_ESPNOW_SEND_LEN);
-    // if (send_param->buffer == NULL) {
-    //     ESP_LOGE(TAG, "Malloc send buffer fail");
-    //     free(send_param);
-    //     vSemaphoreDelete(s_example_espnow_queue);
-    //     esp_now_deinit();
-    //     return ESP_FAIL;
-    // }
-    // memcpy(send_param->dest_mac, s_example_broadcast_mac, ESP_NOW_ETH_ALEN);
-    // //example_espnow_data_prepare(send_param);
-
-    // // example_espnow_send_param_t *send_param = (example_espnow_send_param_t *)pvParameter;
-    // if (esp_now_send(send_param->dest_mac, send_param->buffer, send_param->len) != ESP_OK) {
-    //     ESP_LOGE(TAG, "Send error");
-    //     example_espnow_deinit(send_param);
-    //     vTaskDelete(NULL);
-    // }
-
     return esp_now_send(s_example_broadcast_mac, buffer, len);
 }
--- sender/main/deep_sleep_example_main.c
+++ sender/main/deep_sleep_example_main.c
@@ -15,140 +15,24 @@
 #include "driver/rtc_io.h"
 #include "nvs_flash.h"
 #include "nvs.h"
-#include "deep_sleep_example.h"
 #include "../../common/espnow.h"
-
 
 static const char *TAG = "sender_deep_sleep_example";
 
-// #if SOC_RTC_FAST_MEM_SUPPORTED
-static RTC_DATA_ATTR struct timeval sleep_enter_time;
-// #else
-// static struct timeval sleep_enter_time;
-// #endif
-
 volatile bool send_done = false;
 
-static void deep_sleep_task(void *args)
+const int ext_wakeup_pin_0 = 3;
+void example_deep_sleep_register_ext0_wakeup(int level)
 {
-//     /**
-//      * Prefer to use RTC mem instead of NVS to save the deep sleep enter time, unless the chip
-//      * does not support RTC mem(such as esp32c2). Because the time overhead of NVS will cause
-//      * the recorded deep sleep enter time to be not very accurate.
-//      */
-// #if !SOC_RTC_FAST_MEM_SUPPORTED
-//     // Initialize NVS
-//     esp_err_t err = nvs_flash_init();
-//     if (err == ESP_ERR_NVS_NO_FREE_PAGES || err == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-//         // NVS partition was truncated and needs to be erased
-//         // Retry nvs_flash_init
-//         ESP_ERROR_CHECK(nvs_flash_erase());
-//         err = nvs_flash_init();
-//     }
-//     ESP_ERROR_CHECK(err);
+    printf("Enabling EXT0 wakeup on pin GPIO%d\n", ext_wakeup_pin_0);
+    ESP_ERROR_CHECK(esp_sleep_enable_ext0_wakeup(ext_wakeup_pin_0, level));
 
-//     nvs_handle_t nvs_handle;
-//     err = nvs_open("storage", NVS_READWRITE, &nvs_handle);
-//     if (err != ESP_OK) {
-//         printf("Error (%s) opening NVS handle!\n", esp_err_to_name(err));
-//     } else {
-//         printf("Open NVS done\n");
-//     }
-
-//     // Get deep sleep enter time
-//     nvs_get_i32(nvs_handle, "slp_enter_sec", (int32_t *)&sleep_enter_time.tv_sec);
-//     nvs_get_i32(nvs_handle, "slp_enter_usec", (int32_t *)&sleep_enter_time.tv_usec);
-// #endif
-
-    struct timeval now;
-    gettimeofday(&now, NULL);
-    int sleep_time_ms = (now.tv_sec - sleep_enter_time.tv_sec) * 1000 + (now.tv_usec - sleep_enter_time.tv_usec) / 1000;
-
-    switch (esp_sleep_get_wakeup_cause()) {
-        case ESP_SLEEP_WAKEUP_TIMER: {
-            printf("Wake up from timer. Time spent in deep sleep: %dms\n", sleep_time_ms);
-            break;
-        }
-
-// #if CONFIG_EXAMPLE_GPIO_WAKEUP
-//         case ESP_SLEEP_WAKEUP_GPIO: {
-//             uint64_t wakeup_pin_mask = esp_sleep_get_gpio_wakeup_status();
-//             if (wakeup_pin_mask != 0) {
-//                 int pin = __builtin_ffsll(wakeup_pin_mask) - 1;
-//                 printf("Wake up from GPIO %d\n", pin);
-//             } else {
-//                 printf("Wake up from GPIO\n");
-//             }
-//             break;
-//         }
-// #endif //CONFIG_EXAMPLE_GPIO_WAKEUP
-
-#if CONFIG_EXAMPLE_EXT0_WAKEUP
-        case ESP_SLEEP_WAKEUP_EXT0: {
-            printf("Wake up from ext0\n");
-            break;
-        }
-#endif // CONFIG_EXAMPLE_EXT0_WAKEUP
-
-// #ifdef CONFIG_EXAMPLE_EXT1_WAKEUP
-//         case ESP_SLEEP_WAKEUP_EXT1: {
-//             uint64_t wakeup_pin_mask = esp_sleep_get_ext1_wakeup_status();
-//             if (wakeup_pin_mask != 0) {
-//                 int pin = __builtin_ffsll(wakeup_pin_mask) - 1;
-//                 printf("Wake up from GPIO %d\n", pin);
-//             } else {
-//                 printf("Wake up from GPIO\n");
-//             }
-//             break;
-//         }
-// #endif // CONFIG_EXAMPLE_EXT1_WAKEUP
-
-// #ifdef CONFIG_EXAMPLE_TOUCH_WAKEUP
-//         case ESP_SLEEP_WAKEUP_TOUCHPAD: {
-//             printf("Wake up from touch on pad %d\n", esp_sleep_get_touchpad_wakeup_status());
-//             break;
-//         }
-// #endif // CONFIG_EXAMPLE_TOUCH_WAKEUP
-
-        case ESP_SLEEP_WAKEUP_UNDEFINED:
-        default:
-            printf("Not a deep sleep reset\n");
-    }
-
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
-
-#if CONFIG_IDF_TARGET_ESP32
-    // Isolate GPIO12 pin from external circuits. This is needed for modules
-    // which have an external pull-up resistor on GPIO12 (such as ESP32-WROVER)
-    // to minimize current consumption.
-    rtc_gpio_isolate(GPIO_NUM_12);
-#endif
-
-    printf("Entering deep sleep\n");
-
-    // get deep sleep enter time
-    gettimeofday(&sleep_enter_time, NULL);
-
-// #if !SOC_RTC_FAST_MEM_SUPPORTED
-//     // record deep sleep enter time via nvs
-//     ESP_ERROR_CHECK(nvs_set_i32(nvs_handle, "slp_enter_sec", sleep_enter_time.tv_sec));
-//     ESP_ERROR_CHECK(nvs_set_i32(nvs_handle, "slp_enter_usec", sleep_enter_time.tv_usec));
-//     ESP_ERROR_CHECK(nvs_commit(nvs_handle));
-//     nvs_close(nvs_handle);
-// #endif
-
-    // enter deep sleep
-    esp_deep_sleep_start();
+    // Configure pullup/downs via RTCIO to tie wakeup pins to inactive level during deepsleep.
+    // EXT0 resides in the same power domain (RTC_PERIPH) as the RTC IO pullup/downs.
+    // No need to keep that power domain explicitly, unlike EXT1.
+    ESP_ERROR_CHECK(rtc_gpio_pullup_dis(ext_wakeup_pin_0));
+    ESP_ERROR_CHECK(rtc_gpio_pulldown_en(ext_wakeup_pin_0));
 }
-
-static void example_deep_sleep_register_rtc_timer_wakeup(void)
-{
-    const int wakeup_time_sec = 20;
-    printf("Enabling timer wakeup, %ds\n", wakeup_time_sec);
-    ESP_ERROR_CHECK(esp_sleep_enable_timer_wakeup(wakeup_time_sec * 1000000));
-}
-
-
 
 /* ESPNOW sending or receiving callback function is called in WiFi task.
  * Users should not do lengthy operations from this task. Instead, post
@@ -158,9 +42,6 @@ static void example_espnow_send_cb(const uint8_t *mac_addr, esp_now_send_status_
     send_done = true;
 
     printf("Send cb, mac: " MACSTR ", status: %d\n", MAC2STR(mac_addr), status);
-
-    // example_espnow_event_t evt;
-    // example_espnow_event_send_cb_t *send_cb = &evt.info.send_cb;
 
     if (mac_addr == NULL) {
         ESP_LOGE(TAG, "Send cb arg error");
@@ -178,11 +59,6 @@ static void example_espnow_send_cb(const uint8_t *mac_addr, esp_now_send_status_
     // if (espnow_queue_event(&evt) != pdTRUE) {
     //     ESP_LOGW(TAG, "Send send queue fail");
     // }
-}
-
-void send_button_status()
-{
-
 }
 
 unsigned long millis() {
@@ -209,7 +85,7 @@ void app_main(void)
     ESP_ERROR_CHECK( ret );
 
     example_wifi_init();
-    example_espnow_init(example_espnow_send_cb, NULL, false);
+    example_espnow_init(example_espnow_send_cb, NULL);
 
     uint8_t payload[2] = {0x01, get_button_status() ? 0x01 : 0x00}; 
     if (espnow_broadcast(payload, sizeof(payload)) != ESP_OK) {
@@ -224,30 +100,9 @@ void app_main(void)
     }
 
     printf("Send done, time taken: %lu ms\n", millis() - start);
-    // vTaskDelay(5000 / portTICK_PERIOD_MS);
 
-    /* Enable wakeup from deep sleep by rtc timer */
-    example_deep_sleep_register_rtc_timer_wakeup();
-
-// #if CONFIG_EXAMPLE_GPIO_WAKEUP
-//     /* Enable wakeup from deep sleep by gpio */
-//     example_deep_sleep_register_gpio_wakeup();
-// #endif
-
-#if CONFIG_EXAMPLE_EXT0_WAKEUP
-    /* Enable wakeup from deep sleep by ext0 */
     example_deep_sleep_register_ext0_wakeup(get_button_status() ? 0 : 1);
-#endif
 
-// #if CONFIG_EXAMPLE_EXT1_WAKEUP
-//     /* Enable wakeup from deep sleep by ext1 */
-//     example_deep_sleep_register_ext1_wakeup();
-// #endif
-
-// #if CONFIG_EXAMPLE_TOUCH_WAKEUP
-//     /* Enable wakeup from deep sleep by touch */
-//     example_deep_sleep_register_touch_wakeup();
-// #endif
-
-    xTaskCreate(deep_sleep_task, "deep_sleep_task", 4096, NULL, 6, NULL);
+    printf("Entering deep sleep...\n");
+    esp_deep_sleep_start();
 }
